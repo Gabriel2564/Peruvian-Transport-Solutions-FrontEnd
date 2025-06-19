01608aaeca9e9a214a2f98f5e12f73cabf606cbf
RodrigoGY <u202111052@upc.edu.pe>
2025-06-18 19:50:02 -0500

    cambios 1.0

--- src/app/components/rol/insertarrol/insertarrol.component.ts
+++ src/app/components/rol/insertarrol/insertarrol.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-insertarrol',
   imports: [],
   templateUrl: './insertarrol.component.html',
+  providers: [provideNativeDateAdapter()],
   styleUrl: './insertarrol.component.css'
 })
 export class InsertarrolComponent {
--- src/app/components/rol/listarrol/listarrol.component.ts
+++ src/app/components/rol/listarrol/listarrol.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { Rol } from '../../../models/Rol';
+import { RolService } from '../../../services/Rol.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-listarrol',
-  imports: [],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
   templateUrl: './listarrol.component.html',
   styleUrl: './listarrol.component.css'
 })
 export class ListarrolComponent {
+  dataSource: MatTableDataSource<Rol> = new MatTableDataSource();
+  displayedColumns: string[] = ['c1', 'c2','c3'];
 
+  constructor(private rolService: RolService) {}
+  
+  ngOnInit(): void {
+    this.rolService.list().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+        //Actualiza la tabla automaticamente
+    this.rolService.getList().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
+
+  eliminar(id: number) {
+    this.rolService.deleteI(id).subscribe(() => {
+      this.rolService.list().subscribe(data => {
+        this.rolService.setList(data);
+      });
+    });
+  }
 }
