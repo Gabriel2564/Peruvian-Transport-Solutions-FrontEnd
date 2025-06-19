54aa6a1263cbc5a5a3a049c85e31373d60221c43
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-06-19 12:55:10 -0500

    Actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { PagoComponent } from "./components/pago/pago.component";
 import { ItemComponent } from "./components/item/item.component";
+import { EstadoComponent } from "./components/estado/estado.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [PagoComponent, ItemComponent],
+  standalone: true,
+  imports: [
+    PagoComponent, 
+    ItemComponent, 
+    EstadoComponent, 
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { PagoComponent } from './components/pago/pago.component';
 import { InsertarpagoComponent } from './components/pago/insertarpago/insertarpago.component';
+
 import { ItemComponent } from './components/item/item.component';
 import { InsertaritemComponent } from './components/item/insertaritem/insertaritem.component';
+
+import { EstadoComponent } from './components/estado/estado.component';
+import { InsertarestadoComponent } from './components/estado/insertarestado/insertarestado.component';
+
 
 export const routes: Routes = [
   {
@@ -28,7 +34,19 @@ export const routes: Routes = [
         path:'actualizaciones/:id', component:InsertaritemComponent
       }
     ]
-}
+},
+{
+  path:'rutaEstado', component:EstadoComponent,
+  children:[
+    {
+      path:'insertar', component:InsertarestadoComponent
+    },
+
+    {
+      path:'actualizaciones/:id', component:InsertarestadoComponent
+    }
+  ]
+},
 
 ];
 
