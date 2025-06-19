2a779afb7ce42af96dd684d8772ab79b6cb40bd8
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2025-06-19 12:55:21 -0500

    CRUD-Estado

--- src/app/components/estado/estado.component.ts
+++ src/app/components/estado/estado.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarestadoComponent } from '../estado/listarestado/listarestado.component';
 
 @Component({
   selector: 'app-estado',
-  imports: [],
-  templateUrl: './estado.component.html',
-  styleUrl: './estado.component.css'
+  imports: [RouterOutlet, ListarestadoComponent],
+  templateUrl: './Estado.component.html',
+  styleUrl: './Estado.component.css',
 })
 export class EstadoComponent {
+  constructor(public route:ActivatedRoute) {}
 
 }
--- src/app/components/estado/insertarestado/insertarestado.component.ts
+++ src/app/components/estado/insertarestado/insertarestado.component.ts
@@ -1,11 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Estado } from '../../../models/Estado';
+import { EstadoService } from '../../../services/Estado.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-insertarestado',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
   templateUrl: './insertarestado.component.html',
-  styleUrl: './insertarestado.component.css'
+  styleUrl: './insertarestado.component.css',
 })
-export class InsertarestadoComponent {
+export class InsertarestadoComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  estado: Estado = new Estado();
+  edicion: boolean = false;
+  id: number = 0;
+  types: { value: string; viewValue: string }[] = [
+    { value: 'Disponible', viewValue: 'Disponible' },
+    { value: 'Ocupado', viewValue: 'Ocupado' },
+  ];
 
+  constructor(
+    private aS: EstadoService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = this.id != null;
+      this.init();
+    });
+  }
+
+  aceptar() {
+    if (this.form.valid) {
+      this.estado.idEstado = this.form.getRawValue().id;
+      this.estado.statusTypeEstado = this.form.value.type;
+
+      const request = this.edicion
+        ? this.aS.update(this.estado)
+        : this.aS.insert(this.estado);
+
+      request.subscribe(() => {
+        this.aS.list().subscribe((data) => {
+          this.aS.setList(data);
+          this.router.navigate(['rutaEstado']);
+        });
+      });
+    }
+  }
+
+  init() {
+    if (this.edicion) {
+      this.aS.listId(this.id).subscribe((data) => {
+        this.form = this.formBuilder.group({
+          id: [{ value: data.idEstado, disabled: true }],
+          type: [data.statusTypeEstado, Validators.required],
+        });
+      });
+    } else {
+      this.form = this.formBuilder.group({
+        id: [''],
+        type: ['', Validators.required],
+      });
+    }
+  }
 }
