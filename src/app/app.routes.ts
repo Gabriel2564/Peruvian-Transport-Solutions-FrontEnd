3ffc112f2f24005f49ebad3794e8d003da278e30
rominasososo <u202111487@upc.edu.pe>
2025-06-18 19:49:05 -0500

    cambios 3
    cambios .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { ListarusuarioComponent } from './components/usuario/listarusuario/listarusuario.component';
 
 export const routes: Routes = [
+      
     {
-        path:'',
-        redirectTo:''
-    },
-    {
-        path:'rutausuario',component:UsuarioComponent
+        path:'rutausuario',component:UsuarioComponent,
+        children: [
+            {
+                path:'listarusuario',
+                component:ListarusuarioComponent
+            }
+         ]
     }
 ];
--- src/app/components/usuario/listarusuario/listarusuario.component.ts
+++ src/app/components/usuario/listarusuario/listarusuario.component.ts
@@ -1,26 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { UsuarioService } from '../../../services/Usuario.service';
 import { Usuario } from '../../../models/Usuario';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-listarusuario',
   imports: [
-    MatTableModule,
     CommonModule,
     MatButtonModule,
-    RouterLink,
     MatIconModule,
+    RouterLink,
+    MatTableModule
   ],
   templateUrl: './listarusuario.component.html',
   styleUrl: './listarusuario.component.css'
 })
 export class ListarusuarioComponent implements OnInit{
   dataSource: MatTableDataSource<Usuario> = new MatTableDataSource();
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'];
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6'];
   
   constructor(private uS:UsuarioService){}
 
