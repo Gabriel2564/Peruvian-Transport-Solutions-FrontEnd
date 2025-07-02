c9257f907465e62f16e87792de19d88573eaa568
rominasososo <u202111487@upc.edu.pe>
2025-07-01 21:42:12 -0500

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {  MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
+     MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink
+
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
