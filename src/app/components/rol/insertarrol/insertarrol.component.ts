ca6a90856fe4331598c45ba0a838b67e6451689c
rominasososo <u202111487@upc.edu.pe>
2025-07-05 21:51:31 -0500

    diseño rol
    falta verificar funciones

--- src/app/components/rol/insertarrol/insertarrol.component.ts
+++ src/app/components/rol/insertarrol/insertarrol.component.ts
@@ -1,74 +1,102 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
 import { Rol } from '../../../models/Rol';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Usuario } from '../../../models/Usuario';
 import { RolService } from '../../../services/Rol.service';
+import { UsuarioService } from '../../../services/Usuario.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
-import { Usuario } from '../../../models/Usuario';
-import { UsuarioService } from '../../../services/Usuario.service';
-
 
 @Component({
   selector: 'app-insertarrol',
-  imports: [ MatInputModule,
-    MatFormFieldModule,
+  standalone: true,
+  imports: [
     ReactiveFormsModule,
     CommonModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatButtonModule],
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
+  ],
   templateUrl: './insertarrol.component.html',
-  providers: [provideNativeDateAdapter()],
   styleUrl: './insertarrol.component.css'
 })
-export class InsertarrolComponent implements OnInit{
+export class InsertarrolComponent implements OnInit {
   form: FormGroup = new FormGroup({});
   rol: Rol = new Rol();
   edicion: boolean = false;
-  id: number = 0
+  id: number = 0;
 
-  opciones:{value:string,viewValue:string}[]=[
-    {value:'ADMINISTRADOR',viewValue:'ADMINISTRADOR'},
-    {value:'TURISTA',viewValue:'TURISTA'},
-    {value:'CONDUCTOR',viewValue:'CONDUCTOR'},
-  ]
-
-  listaUsuarios:Usuario[]=[]
+  listaUsuarios: Usuario[] = [];
+  opciones = [
+    { value: 'ADMINISTRADOR', viewValue: 'ADMINISTRADOR' },
+    { value: 'TURISTA', viewValue: 'TURISTA' },
+    { value: 'CONDUCTOR', viewValue: 'CONDUCTOR' }
+  ];
 
   constructor(
     private rS: RolService,
     private formBuilder: FormBuilder,
-    private router: Router,  
-    private uS:UsuarioService
+    private router: Router,
+    private route: ActivatedRoute,
+    private uS: UsuarioService
   ) {}
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      nombre: ['', Validators.required],
-      usu: ['', Validators.required],
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.edicion = params['id'] != null;
+      this.init();
     });
-    this.uS.list().subscribe(data=>{
-      this.listaUsuarios=data
-    })
+
+    this.form = this.formBuilder.group({
+      id: [''],
+      nombre: ['', Validators.required],
+      usuario: ['', Validators.required]
+    });
+
+    this.uS.list().subscribe(data => {
+      this.listaUsuarios = data;
+    });
   }
 
- aceptar() {
+  aceptar() {
     if (this.form.valid) {
+      this.rol.id = this.form.value.id;
       this.rol.rol = this.form.value.nombre;
-      this.rol.usuario.id = this.form.value.usu;
-      this.rS.insert(this.rol).subscribe((data) => {
-        this.rS.list().subscribe((data) => {
+      this.rol.usuario = { id: this.form.value.usuario } as Usuario;
+
+      const request = this.edicion
+        ? this.rS.update(this.rol)
+        : this.rS.insert(this.rol);
+
+      request.subscribe(() => {
+        this.rS.list().subscribe(data => {
           this.rS.setList(data);
+          this.router.navigate(['rutaRol']);
         });
       });
-      this.router.navigate(['rutaRol'])
     }
   }
 
+  init() {
+    if (this.edicion) {
+      this.rS.listId(this.id).subscribe(data => {
+        this.form = this.formBuilder.group({
+          id: [data.id],
+          nombre: [data.rol, Validators.required],
+          usuario: [data.usuario.id, Validators.required]
+        });
+      });
+    }
+  }
 }
